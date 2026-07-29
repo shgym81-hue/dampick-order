e9f77d1cb142575350f6a9b97b1a1ce8335cf3fa
shgym81-hue <shgym81@gmail.com>
2026-07-29 19:30:16 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,46 +1,67 @@
-const CACHE_NAME = "dampick-admin-v1";
-const APP_FILES = [
-  "./",
-  "./admin.html",
-  "./config.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png",
-  "./apple-touch-icon.png",
-  "./favicon.png",
-  "./dampick-logo.png"
-];
+const CACHE_NAME = "dampick-v4";
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES))
-  );
+self.addEventListener("install", function () {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", function (event) {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys
-          .filter((key) => key !== CACHE_NAME)
-          .map((key) => caches.delete(key))
-      )
-    )
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
+
   self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  const requestUrl = new URL(event.request.url);
+
+  if (requestUrl.origin !== self.location.origin) {
+    return;
+  }
 
   event.respondWith(
     fetch(event.request)
-      .then((response) => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+      .then(function (response) {
+        if (
+          response &&
+          response.status === 200
+        ) {
+          const responseCopy =
+            response.clone();
+
+          caches
+            .open(CACHE_NAME)
+            .then(function (cache) {
+              cache.put(
+                event.request,
+                responseCopy
+              );
+            });
+        }
+
         return response;
       })
-      .catch(() => caches.match(event.request))
+      .catch(function () {
+        return caches
+          .match(event.request)
+          .then(function (cachedResponse) {
+            return (
+              cachedResponse ||
+              Response.error()
+            );
+          });
+      })
   );
 });
